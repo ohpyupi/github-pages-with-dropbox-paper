6cd3c565826b2b71cfe2d41f8448be84d686d781
ohpyupi <hoseong.a.lee@gmail.com>
2022-07-27 18:58:29 +0000

    Update from https://github.com/ohpyupi/github-pages-with-dropbox-paper/commit/c30c93d8c7f550d56772246fa884d5102e938d2e

--- scripts/env.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    DROPBOX_GITHUB_PAGES_FOLDER: process.env.DROPBOX_GITHUB_PAGES_FOLDER,
-    DROPBOX_AUTH_REFRESH_TOKEN: process.env.DROPBOX_AUTH_REFRESH_TOKEN,
-    DROPBOX_AUTH_APP_KEY: process.env.DROPBOX_AUTH_APP_KEY,
-    DROPBOX_AUTH_APP_SECRET: process.env.DROPBOX_AUTH_APP_SECRET
-};
